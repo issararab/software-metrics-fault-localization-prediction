34fcd02b192e604d5a14af10acdfed9b2f47ce65
issar <issar.arab@gmail.com>
2025-05-01 08:06:47 +0200

    Add dependencies

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install numpy==1.23.5
+pip install torch==2.1.2
+pip install pandas==2.1.4
+pip install scikit-learn==1.3.1
+pip install seaborn==0.13.2
+pip install matplotlib==3.8.2
+pip install xgboost==2.0.3
+pip install lightgbm==4.5.0
